d98aa114224e83ce324df076d2f5857bb4e5231e
mike <mikemayer67@vmwishes.com>
2019-02-27 08:50:15 -0500

    Added IconButton

---
+++ SudokuWizard/UIElements/IconButton.swift
@@ -0,0 +1,21 @@
+//
+//  IconButton.swift
+//  SudokuWizard
+//
+//  Created by Mike Mayer on 2/24/19.
+//  Copyright © 2019 VMWishes. All rights reserved.
+//
+
+import UIKit
+
+class IconButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
